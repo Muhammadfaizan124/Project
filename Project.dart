9ee2db5ab3f51ab43699f706349c437ad5ba6565
Muhammadfaizan124 <faizanislam124@gmail.com>
2024-02-18 19:54:16 +0500

    in process

--- Project.dart
+++ Project.dart
@@ -1,3 +1,12 @@
+import 'dart:io';
 void main(){
-  print ("first project of flutter");
+  print ("Welcome to hyper technologies of vehicles.");
+  print ("Technologies that you won't find in every vehicle.");
+  print ("Please let us know about yourself then we will dive in it");
+   
+  print ("Enter your name");
+  //string? name = stdin.readLineSync();
+
+
+
 }
