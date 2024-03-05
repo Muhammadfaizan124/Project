2b602364e5b341239df65d19245cd483b83dcca9
Muhammadfaizan124 <faizanislam124@gmail.com>
2024-03-06 00:43:48 +0500

    few changes

--- cropproducts.dart
+++ cropproducts.dart
@@ -6,11 +6,23 @@ void cropProducts() {
   print("2. coffee beans");
   print("3. flavour");
 
-  Map products = {1: "rice", 2: "coffee beans", 3: "flavour"};
+  Map products = {
+    1: {"rice": 550},
+    2: {"coffee beans": 800},
+    3: {"flavour": 150}
+  };
 
   var selectcropProduct = stdin.readLineSync();
-
+  //rice
   if (selectcropProduct == "1") {
-    print("${products[1]} Rs 550 per kg");
+    print("${products[1]} Rs per kg");
+  }
+  //coffe beans
+  else if (selectcropProduct == "2") {
+    print("${products[2]} Rs per kg");
+  }
+  //flavour
+  else if (selectcropProduct == "3") {
+    print("${products[3]} Rs per kg");
   }
 }
