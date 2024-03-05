4f8ba84fc67e9d13d474b5409692056e0b1bfaa3
Muhammadfaizan124 <faizanislam124@gmail.com>
2024-03-05 23:09:50 +0500

    few changes

--- meat.dart
+++ meat.dart
@@ -4,21 +4,21 @@ import 'foodstuffs.dart';
 
 void meat() {
   //meat.
-  print("Available items");
-  print("1. chicken");
-  print("2. beef");
-  print("3. mutton");
-  print("0. Go Back");
   while (true) {
+    print("Available items");
+    print("1. chicken");
+    print("2. beef");
+    print("3. mutton");
+    print("0. Go Back");
     //selecting meat.
     var selectMeat = stdin.readLineSync();
 
     //chicken
     if (selectMeat == "1") {
-      print("1. chicken boneless Rs 900 per kg");
-      print("2. scrag Rs 600 per kg");
-      print("0. Go back");
       while (true) {
+        print("1. chicken boneless Rs 900 per kg");
+        print("2. scrag Rs 600 per kg");
+        print("0. Go back");
         var selectType = stdin.readLineSync();
 
         if (selectType == "1") {
@@ -38,10 +38,10 @@ void meat() {
     }
     //beef
     else if (selectMeat == "2") {
-      print("1. beef boneless Rs 1500 per kg");
-      print("2. scrag Rs 900 per kg");
-      print("0. Go back");
       while (true) {
+        print("1. beef boneless Rs 1500 per kg");
+        print("2. scrag Rs 900 per kg");
+        print("0. Go back");
         var selectType = stdin.readLineSync();
 
         if (selectType == "1") {
@@ -61,10 +61,10 @@ void meat() {
     }
     //mutton
     else if (selectMeat == "3") {
-      print("1. mutton boneless Rs 2000 per kg");
-      print("2. scrag Rs 1200 per kg");
-      print("0. Go back");
       while (true) {
+        print("1. mutton boneless Rs 2000 per kg");
+        print("2. scrag Rs 1200 per kg");
+        print("0. Go back");
         var selectType = stdin.readLineSync();
 
         if (selectType == "1") {
